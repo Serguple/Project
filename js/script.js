61f50c803cc14474ed8305dd6bc6b0f4970a47ed
Serhii <sergy200478@gmail.com>
2023-10-21 10:32:30 +0300

    if switch fun

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-let a = "b";
+"use strict";
+
+let num = 19;
+
+// if (num < 50) {
+//     console.log("Too little")
+// } else if (num > 100) {
+//     console.log("Too much")
+// } else {
+//     console.log("OK!!!")
+// }
+
+switch (num) {
+    case 42:
+        console.log("Error");
+        break;
+    case 857:
+        console.log("Error");
+        break;
+    case 199:
+        console.log("Yes!!!");
+        break;
+    default: 
+        console.log("Unlucky");
+};
