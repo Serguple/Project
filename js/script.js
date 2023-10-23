8a271a1efb3262ecc648322a4e50995173136b7a
Serhii <sergy200478@gmail.com>
2023-10-23 19:54:02 +0300

    while practice

--- js/script.js
+++ js/script.js
@@ -1,25 +1,57 @@
-"use strict";
+/* Задание на урок:
 
-let result = '',
-    length = 7;
-    
-for (let i = 1; i < length; i++) {
-    for (let j = 0; j < i; j++){
-        result += '*';
-    };
-    result += "\n"
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+'use strict';
+
+// Код возьмите из предыдущего домашнего задания
+
+
+let numberOfFilms = +prompt("How many films have you watched?", '');
+
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-console.log(result)
-
-
-firts: for (let i = 0; i < 3; i++) {
-    console.log(i)
-    for (let j = 0; j < 3; j++) {
-        console.log(j)
-        for (let k = 0; k < 3; k++) {
-            if (k === 2) break firts;
-            console.log(k)
-        }
-    }
+if (personalMovieDB.count < 10 && personalMovieDB.count >= 0) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count <= 30 && personalMovieDB.count >= 10) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
 }
+
+let l = 0;
+
+do {
+    const watchedFilmName = prompt("One of the watched movies?", ''),
+    watchedFilmRating = +prompt("This film's rating?", '');
+
+    if (watchedFilmName === "" || watchedFilmName.length > 50) {
+        l = 0;
+        continue;
+    }
+    personalMovieDB.movies[watchedFilmName] = watchedFilmRating;
+    
+    l++;
+} while (l < 2)
+
+console.log(personalMovieDB.count);
+console.log(personalMovieDB.movies);
